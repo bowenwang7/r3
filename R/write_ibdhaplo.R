96cc5daed59e377c98af405f1ac647a7b7041f29
Bowen Wang <bowenw54@Bowens-iMac.local>
2018-04-03 22:48:51 -0700

    Fix write_ibdhaplo example line "too long".

--- R/write_ibdhaplo.R
+++ R/write_ibdhaplo.R
@@ -2,32 +2,35 @@
 #' 
 #' \code{write.ibdhaplo} prepares the marker data file for running IBDHAPLO.
 #' 
-#' The input marker data needs to be subject/haplotype by marker/allele. For example, suppose \code{data} is a 4x10 matrix, use \code{input.counts = TRUE} if \code{data} contains counts of reference alleles of 4 individuals at 10 markers; use \code{input.haplotype = TRUE} if \code{data} contains allelic types of 4 haplotypes at 10 markers; use default options if \code{data} contains allelic types of 4 individuals at 5 markers.
+#' The input marker data needs to be subject/haplotype by marker/allele. For example, suppose \code{data} is a 4x10 matrix, use \code{input.allele = FALSE} if \code{data} contains counts of reference alleles of 4 individuals at 10 markers; use \code{input.haplotype = TRUE} if \code{data} contains allelic types of 4 haplotypes at 10 markers; use default options if \code{data} contains allelic types of 4 individuals at 5 markers.
 #' 
 #' @param marker numeric vector, marker genetic positions in cM.
 #' @param freq numeric vector, marker reference allele frequencies.
 #' @param data numeric matrix, genetic marker data.
 #' @param member string vector, member ID.
-#' @param input.counts logical, default FALSE.
+#' @param input.allele logical, default TRUE.
 #' @param input.haplotype logical, default FALSE.
 #' @param outfile string, output file name.
 #' @references MORGAN Tutorial, \url{https://www.stat.washington.edu/thompson/Stuff/morgan-tut.html}.
 #' @references Brown et al. (2012) Genetics 190:1447-1460, \url{https://www.ncbi.nlm.nih.gov/pubmed/22298700}.
 #' 
 #' @examples 
+#' \dontrun{
 #' nsnp = 7 # number of SNPs
 #' freq = runif(nsnp, 0.05, 0.95)
 #' nhaplo = 4 # number of founder haplotypes
 #' haplotype = sim.haplotype(freq, nhaplo)
 #' marker = sort(runif(7,0,100))
-#' write.ibdhaplo(marker, freq, haplotype, member = c("ind1", "ind2"), input.haplotype = TRUE, outfile = "./ibdhaplo.markers")
+#' write.ibdhaplo(marker, freq, haplotype, member = c("ind1", "ind2"), 
+#' input.haplotype = TRUE, outfile = "./ibdhaplo.markers")
+#' }
 #' @export
-write.ibdhaplo = function(marker, freq, data, member, input.counts = FALSE, input.haplotype = FALSE, outfile = NULL){
+write.ibdhaplo = function(marker, freq, data, member, input.allele = TRUE, input.haplotype = FALSE, outfile = NULL){
   if(is.null(outfile)){
     stop("Please specify an output file.")
   }
   
-  if(input.counts){
+  if(!input.allele){
     genotype = t(apply(data, 1, function(x) c(sapply(x, function(y) c(rep(1,y),rep(2,2-y))))))
   }else{
     if(input.haplotype){
